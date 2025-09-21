2f43331f70022fd53cf2566888f1127d17902d24
Somaya Medhat <somayamedhat179@gmail.com>
2025-09-21 14:51:01 +0200

    first commit

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 async function getAPI (){
     var result =await fetch("https://dummyjson.com/products") ;
     var json =await result.json() ;
-    var recipes=json.products ; 
+    var products=json.products ; 
     var myText = "";
 
-      for (var i = 0; i < recipes.length; i++) {
+      for (var i = 0; i < products.length; i++) {
         var box = `
           <div class="col-md-4 text-center my-3" >
-            <img src="${recipes[i].images[0]}"class="w-100" style="height:350px">
-            <h6>${recipes[i].title}</h6>
-            <h5>${recipes[i].category}</h5>
-            <h5>${recipes[i].price}$</h5>
-            <h5>${recipes[i].rating}</h5>
-            <h5>${recipes[i].id}</h5>
+            <img src="${products[i].images[0]}"class="w-100" style="height:350px">
+            <h6>${products[i].title}</h6>
+            <h5>${products[i].category}</h5>
+            <h5>${products[i].price}$</h5>
+            <h5>${products[i].rating}</h5>
+            <h5>${products[i].id}</h5>
           </div> 
         `;
         myText += box; 
